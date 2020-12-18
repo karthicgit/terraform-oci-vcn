090837ecc2c392fd02af209aeebc30f6000c99a2
Karthic Ravindran <karthic.ravindran@oracle.com>
2020-12-18 21:51:10 +0530

    Update apply_test.go

--- test/apply_test.go
+++ test/apply_test.go
@@ -28,7 +28,7 @@ func TestTerraformAwsHelloWorldExample(t *testing.T) {
 	// Run `terraform output` to get the IP of the instance
 	//bastionIp := terraform.Output(t, terraformOptions, "bastion_public_ip")
 
-	// Make   and make sure we get back a 200 OK with the body "Hello, World!"
+	// Make   sure we get back a 200 OK with the body "Hello, World!"
 	//fmt.Println(bastionIp)
 // 	shell.RunCommandAndGetStdOut(t *testing.T,command )
  }
