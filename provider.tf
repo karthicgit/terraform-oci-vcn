64c6b314ccc75f2898359e9805581c47cdd7aacc
Karthic Ravindran <karthic.ravindran@oracle.com>
2021-01-06 14:21:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key      = var.private_key
   region           = var.region
 }
 
@@ -16,4 +16,4 @@ variable "fingerprint" {
 }
 
 
-variable "private_key_path" {}
+variable "private_key" {}
